9d8febecf225e1ce2c415bafa8ba101df7c7cc1c
KhalilMS3 <kmsfouk@hiof.no>
2024-03-22 03:35:07 +0100

    React, Sass and Routing installation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <img className='logo' src="./src/assets/logo.svg" alt="BookSeekr logo" />
+    </>
+  )
+}
+
+export default App
